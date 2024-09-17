78a6557e8492fdd67242bcc006cb45c682af265b
MariaFernanda1818 <83258884+MariaFernanda1818@users.noreply.github.com>
2024-09-17 11:17:55 -0500

    feature-fix: Se agrega base del ms de usuarios

---
+++ src/test/java/com/sistema/sah/usuarios/UsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.sah.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
